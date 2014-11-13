cc8450e73fa16ee6f30c0fff011861eee3f5f615
James E. Marca <james@activimetrics.com>
2014-11-13 08:46:28 -0800

    adding old files that weren't in the repo
    shot myself in the foot by deleting my "configurator" program...

---
+++ test/test_get_county_detector_from_sql.js
@@ -0,0 +1,67 @@
+/*global require before after describe it emit console */
+
+
+var should = require('should')
+var fs = require('fs')
+
+var _ = require('lodash')
+var async = require('async')
+
+var config_okay = require('config_okay')
+var get_records = require('../lib/get_county_overlap_from_sql')
+
+var config={}
+var utils = require('./utils')
+var task
+var test_type ='counties'
+var test_name = '06029' // Kern
+var test_year = 2009
+var path = require('path')
+var rootdir = path.normalize(__dirname)
+var config_file = rootdir+'/../test.config.json'
+
+console.log(config_file)
+before(function(done){
+    config_okay(config_file,function(err,c){
+        config = c
+        get_records.configure(config)
+        return done()
+    })
+    return null
+})
+
+describe('get county detectors list',function(){
+
+    it('should grab detector data for kern, 2007',function(done){
+        get_records.query(test_name,2007,function(e,r){
+            should.not.exist(e)
+            should.exist(r)
+            r.should.have.lengthOf(4)
+            done()
+        })
+    })
+    it('should grab detector data for kern, 2008',function(done){
+        get_records.query(test_name,2008,function(e,r){
+            should.not.exist(e)
+            should.exist(r)
+            r.should.have.lengthOf(4)
+            done()
+        })
+    })
+    it('should grab detector data for kern, 2009',function(done){
+        get_records.query(test_name,2009,function(e,r){
+            should.not.exist(e)
+            should.exist(r)
+            r.should.have.lengthOf(4)
+            done()
+        })
+    })
+    it('should grab detector data for kern, 2009',function(done){
+        get_records.query(test_name,2010,function(e,r){
+            should.not.exist(e)
+            should.exist(r)
+            r.should.have.lengthOf(4)
+            done()
+        })
+    })
+})
